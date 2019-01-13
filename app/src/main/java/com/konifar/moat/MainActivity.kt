64fc026d19496d8e331c2710d3c49978425aa84e
konifar <yahpeycoy0403@gmail.com>
2019-01-14 04:47:14 +0900

    Changed theme

--- app/src/main/java/com/konifar/moat/MainActivity.kt
+++ app/src/main/java/com/konifar/moat/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.konifar.moat
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -10,10 +14,17 @@ import com.konifar.moat.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val PREF_KEY_CONFIG = "pref_key_config"
+    }
+
     private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setUpTheme()
+
         binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
         actionBar?.apply {
             title = getString(R.string.app_name)
@@ -21,7 +32,60 @@ class MainActivity : AppCompatActivity() {
 
         setUpTabs()
 
-        binding.catOne.isSelected = true
+        setUpFooter()
+    }
+
+    private fun setUpTheme() {
+        val config = getCurrentConfig()
+        setTheme(config.themeResId)
+    }
+
+    @SuppressLint("ApplySharedPref")
+    private fun saveConfig(config: Config) {
+        val pref = getSharedPreferences("default", Context.MODE_PRIVATE)
+        pref.edit().putString(PREF_KEY_CONFIG, config.toString()).commit()
+    }
+
+    private fun getCurrentConfig(): Config {
+        val pref = getSharedPreferences("default", Context.MODE_PRIVATE)
+        val configString = pref.getString(PREF_KEY_CONFIG, Config.CAT_ONE.toString())
+        return if (configString != null) {
+            Config.valueOf(configString)
+        } else {
+            Config.CAT_ONE
+        }
+    }
+
+    private fun setUpFooter() {
+        clearTheme()
+        val config = getCurrentConfig()
+        binding.themeIcons.getChildAt(config.index).isSelected = true
+
+        val count = binding.themeIcons.childCount
+        for (i in 0 until count) {
+            val view = binding.themeIcons.getChildAt(i)
+            view.setOnClickListener {
+                clearTheme()
+                it.isSelected = true
+                saveConfig(Config.from(i))
+                restart()
+            }
+        }
+    }
+
+    private fun restart() {
+        Handler().postDelayed({
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }, 300)
+    }
+
+    private fun clearTheme() {
+        val count = binding.themeIcons.childCount
+        for (i in 0 until count) {
+            binding.themeIcons.getChildAt(i).isSelected = false
+        }
     }
 
     private fun setUpTabs() {
@@ -56,5 +120,26 @@ class MainActivity : AppCompatActivity() {
             activity.getString(tabs[position].titleResId)
 
         override fun getCount(): Int = tabs.count()
+    }
+
+    enum class Config(
+        val index: Int,
+        val themeResId: Int
+    ) {
+
+        CAT_ONE(0, R.style.AppTheme),
+        CAT_TWO(1, R.style.AppTheme_CatTwo),
+        CAT_COLORFUL(2, R.style.AppTheme_AppCompat_Vivid);
+
+        companion object {
+            fun from(index: Int): Config {
+                return when (index) {
+                    0 -> CAT_ONE
+                    1 -> CAT_TWO
+                    2 -> CAT_COLORFUL
+                    else -> throw IllegalArgumentException("$index is not supported.")
+                }
+            }
+        }
     }
 }
