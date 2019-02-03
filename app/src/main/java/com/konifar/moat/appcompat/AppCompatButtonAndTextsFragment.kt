d8f6f46ea53ab99dc16f1551dc09eaae709bd1f1
konifar <yahpeycoy0403@gmail.com>
2019-02-03 16:02:54 +0900

    WIP

---
+++ app/src/main/java/com/konifar/moat/appcompat/AppCompatButtonAndTextsFragment.kt
@@ -0,0 +1,26 @@
+package com.konifar.moat.appcompat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.konifar.moat.R
+import com.konifar.moat.databinding.AppcompatButtonsAndTextsFragmentBinding
+
+class AppCompatButtonAndTextsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AppCompatButtonAndTextsFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: AppcompatButtonsAndTextsFragmentBinding = DataBindingUtil.inflate(layoutInflater, R.layout.appcompat_buttons_and_texts_fragment, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/konifar/moat/appcompat/CatGreyActivity.kt
@@ -0,0 +1,122 @@
+package com.konifar.moat.appcompat
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.konifar.moat.R
+import com.konifar.moat.ThemeConfigManager
+import com.konifar.moat.databinding.CatGreyActivityBinding
+
+/**
+ * Colors theme
+ */
+class CatGreyActivity : AppCompatActivity() {
+
+    companion object {
+        const val REPOSITORY_URL = "https://github.com/konifar/master-of-android-theme"
+    }
+
+    private lateinit var binding: CatGreyActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setUpTheme()
+        binding = DataBindingUtil.setContentView(this, R.layout.cat_grey_activity)
+        setUpTabs()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menu_github -> {
+                showBrowser(REPOSITORY_URL)
+                true
+            }
+            R.id.menu_oss -> {
+                OssLicensesMenuActivity.setActivityTitle(getString(R.string.oss_license_title))
+                startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun showBrowser(url: String) {
+        CustomTabsIntent.Builder()
+            .setShowTitle(true)
+            .build()
+            .launchUrl(this, Uri.parse(url))
+    }
+
+    private fun setUpTheme() {
+        val config = ThemeConfigManager.getCurrentConfig(this)
+        changeDarkMode(config.darkMode)
+    }
+
+    private fun changeDarkMode(darkMode: Boolean) {
+        Handler().postDelayed({
+            if (darkMode) {
+                delegate.setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                delegate.setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }, 200)
+    }
+
+    private fun setUpTabs() {
+        binding.viewPager.adapter = PagerAdapter(this)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+    }
+
+    data class PagerAdapter(
+        val activity: AppCompatActivity
+    ) : FragmentStatePagerAdapter(activity.supportFragmentManager) {
+
+        private val tabs = arrayListOf(
+            Tab.ButtonsAndTexts(),
+            Tab.Components(),
+            Tab.Colors()
+        )
+
+        sealed class Tab(@StringRes val titleResId: Int) {
+            abstract val fragment: Fragment
+
+            class ButtonsAndTexts : Tab(R.string.buttons_and_texts_title) {
+                override val fragment = AppCompatButtonAndTextsFragment.newInstance()
+            }
+
+            class Components : Tab(R.string.components_title) {
+                override val fragment = AppCompatComponentsFragment.newInstance()
+            }
+
+            class Colors : Tab(R.string.colors_title) {
+                override val fragment = AppCompatColorsFragment.newInstance()
+            }
+        }
+
+        override fun getItem(position: Int): Fragment = tabs[position].fragment
+
+        override fun getPageTitle(position: Int): CharSequence? =
+            activity.getString(tabs[position].titleResId)
+
+        override fun getCount(): Int = tabs.count()
+    }
+
+}
